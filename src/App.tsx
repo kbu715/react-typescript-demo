9cf8a9aae4bbcd9fe91a295b64e094774dda03a8
kbu715 <kbu715@gmail.com>
2021-10-03 00:25:01 +0900

    ✨ React.ReactNode type

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import './App.css';
+import { Ballondor } from './components/Ballondor';
+import { Heading } from './components/Heading';
 import { Status } from './components/Status';
 
 function App() {
   return (
     <div className="App">
       <Status status="loading" />
+      <Heading>HEYHEYHEY</Heading>
+      <Ballondor><Heading>Ballon-dor goes to Lionel Messi!</Heading></Ballondor>
     </div>
   );
 }
---
+++ src/components/Ballondor.tsx
@@ -0,0 +1,9 @@
+type BallondorProps = {
+  children: React.ReactNode
+}
+
+export const Ballondor = ({children}: BallondorProps) => {
+  return (
+    <div>{children}</div>
+  )
+}
