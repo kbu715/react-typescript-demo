37ce4bb7fd6d52cffb892d42154f12ecc85be5bc
kbu715 <kbu715@gmail.com>
2021-10-03 00:09:46 +0900

    ✨ Introduce union type

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,10 @@
 import './App.css';
-import { Greet } from './components/Greet';
-import { Person } from './components/Person';
-import { PersonList } from './components/PersonList';
+import { Status } from './components/Status';
 
 function App() {
-  const personName = {
-    first: "Bruce",
-    last: "Wayne"
-  }
-
-  const nameList = [
-    {
-      first: "Lionel",
-      last: "Messi"
-    },
-    {
-      first: "Cristiano",
-      last: "Ronaldo"
-    },
-    {
-      first: "Heungmin",
-      last: "Son"
-    }
-  ]
-
   return (
     <div className="App">
-      <Greet name="xxx" messageCount={15} isLoggedIn={true} />
-      <Person name={personName} />
-      <PersonList nameList={nameList} />
+      <Status status="loading" />
     </div>
   );
 }
