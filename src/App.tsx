638ef4cf70440bbe0f4d67db06d9f6a7c995c321
kbu715 <kbu715@gmail.com>
2021-10-03 22:13:06 +0900

    ✨ Generic Props with React TypeScript

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,37 @@
 import './App.css';
-import { Private } from './components/auth/Private';
-import { Profile } from './components/auth/Profile';
+import { List } from './components/generics/List';
 
 function App() {
   return (
     <div className="App">
-      <Private isLoggedIn={true} component={Profile} />
+      {/* <List
+        items={['Batman', 'Superman', 'Wonder Woman']}
+        onClick={item => console.log(item)}
+      /> */}
+      {/* <List items={[1, 2, 3]} onClick={item => console.log(item)} /> */}
+      <List
+        items={[
+          {
+            id: 1,
+            first: 'Bruce',
+            last: 'Wayne',
+            address: 'LA'
+          },
+          {
+            id: 2,
+            first: 'Clark',
+            last: 'Kent',
+            address: 'Tokyo'
+          },
+          {
+            id: 3,
+            first: 'Princess',
+            last: 'Diana',
+            address: 'Seoul'
+          }
+        ]}
+        onClick={item => console.log(item)}
+      />
     </div>
   );
 }
