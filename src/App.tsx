21ce4d812cb89514e932ea591d07ff3ef524c1c4
kbu715 <kbu715@gmail.com>
2021-10-03 16:28:54 +0900

    ✨ Event props with typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
 import './App.css';
-import { Ballondor } from './components/Ballondor';
-import { Greet } from './components/Greet';
-import { Heading } from './components/Heading';
-import { Status } from './components/Status';
+import { Button } from './components/Button';
+import { Input } from './components/Input';
 
 function App() {
   return (
     <div className="App">
-      <Status status="loading" />
-      <Heading>HEYHEYHEY</Heading>
-      <Ballondor><Heading>Ballon-dor goes to Lionel Messi!</Heading></Ballondor>
-      <Greet name="Paul" isLoggedIn={true} />
+      <Button handleClick={(event, id) => {
+        console.log('Button clicked', event, id);
+      }} />
+      <Input value="" handleChange={(event) => console.log(event)} />
     </div>
   );
 }
