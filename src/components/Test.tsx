82eaefae01a866bf4a57292c1f2b72cf37632b2b
kbu715 <kbu715@gmail.com>
2021-10-05 18:21:06 +0900

    ⚗️ useEffect test

--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -1,26 +1,19 @@
-import React, { useState } from "react";
+import { useState, useEffect } from "react";
 
-function Test() {
-  const [count, setCount] = useState<number>(0);
-
-  function incrementCount() {
-    // setCount((count) => count + 1);
-    // setCount((count) => count + 1);
-
-    /* 
-    리액트는 불필요한 재렌더링을 최대한 줄이기 위해 여러 개의 업데이트 함수를 한번에 처리하는데, 
-    이 때 동일한 업데이트 함수가 존재할 때는 나중에 사용된 업데이트 함수의 인자로 상태를 변경한다.
-    */
-    setCount(count + 1);
-    setCount(count + 1);
-  }
-
-  return (
-    <>
-      <span>{count}</span>
-      <button onClick={incrementCount}>+</button>
-    </>
-  );
+const Test = () => {
+  const [windowScrollY, setWindowScrollY] = useState<number>(window.scrollY);
+  const handleScroll = () => {
+    setWindowScrollY(window.scrollY);
+  };
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    console.log("effect");
+    return () => {
+      window.removeEventListener("resize", handleScroll);
+      console.log("clean up");
+    };
+  }, [windowScrollY]);
+  return <div style={{ height: '500vh' }}>{windowScrollY}</div>;
 }
 
 export default Test;
