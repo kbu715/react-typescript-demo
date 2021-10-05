08a220236792be66165730f3b8806430a476a849
kbu715 <kbu715@gmail.com>
2021-10-05 18:20:47 +0900

    ✨ Restricting Props with React and TypeScript

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,10 @@
 import './App.css';
-import Test from './components/Test';
-// import { List } from './components/generics/List';
+import { RandomNumber } from './components/restriction/RandomNumber';
 
 function App() {
   return (
     <div className="App">
-      {/* <List
-        items={['Batman', 'Superman', 'Wonder Woman']}
-        onClick={item => console.log(item)}
-      /> */}
-      {/* <List items={[1, 2, 3]} onClick={item => console.log(item)} /> */}
-      {/* <List
-        items={[
-          {
-            id: 1,
-            first: 'Bruce',
-            last: 'Wayne',
-            address: 'LA'
-          },
-          {
-            id: 2,
-            first: 'Clark',
-            last: 'Kent',
-            address: 'Tokyo'
-          },
-          {
-            id: 3,
-            first: 'Princess',
-            last: 'Diana',
-            address: 'Seoul'
-          }
-        ]}
-        onClick={item => console.log(item)}
-      /> */}
-
-      <Test />
+      <RandomNumber value={10} isPositive />
     </div>
   );
 }
