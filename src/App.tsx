a8a5240f675777d46ee769c765b0219adf9b218e
kbu715 <kbu715@gmail.com>
2021-10-11 11:28:39 +0900

    ✨ Polymorphic Components with React and TypeScript

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css';
-import { CustomComponent } from './components/html/CustomComponent';
+import { Text } from './components/polymorphic/Text';
 
 function App() {
   return (
     <div className="App">
-      <CustomComponent isLoggedIn={true} name="Paul" />
+      <Text as="h3" size="sm">sm</Text>
+      <Text as="h2" size="md">md</Text>
+      <Text as="h1" size="lg">lg</Text>
     </div>
   );
 }
